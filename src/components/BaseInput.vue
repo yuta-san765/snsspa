64e1d9f851c9fc1865f71c3cd2199855bfc2ee30
yuta-san765 <nitoplus765@gmail.com>
2020-05-05 13:48:24 +0900

    Create BaseInput

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="input-wrapper">
+    <label v-if="label" class="input-label">{{label}}</label>
+    <input :value="value" @input="updateValue" v-bind="$attrs" class="input" />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: [String, Number],
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.input-label {
+  display: inline-block;
+  font-size: 15px;
+  font-weight: 600;
+  margin-bottom: 6px;
+}
+.input-wrapper {
+  margin-bottom: 18px;
+}
+.input {
+  width: calc(100% - 10px);
+  height: 54px;
+  padding-left: 10px;
+  border-radius: 15px;
+  border: 1px solid #707070;
+  &:focus {
+    outline: 0;
+    border: 1px solid white !important;
+    box-shadow: 0 0 5px rgba($color: #34aa22, $alpha: 1);
+  }
+}
+::placeholder {
+  color: #000000;
+  opacity: 0.3;
+}
+</style>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,19 +1,51 @@
 <template>
   <div class="sign-up">
     <div class="header">
-      <div class="header__left"></div>
+      <div class="header__left">
+        <font-awesome-icon
+          class="icon icon-left"
+          icon="angle-left"
+        ></font-awesome-icon>
+      </div>
       <div class="header__center">
         <h3 class="header__title">ユーザー登録</h3>
       </div>
-      <div class="header__right"></div>
+      <div class="header__right">
+        <font-awesome-icon class="icon icon-pen" icon="pen"></font-awesome-icon>
+      </div>
+    </div>
+    <div class="sign-up-form">
+      <form>
+        <label for="upload-picture" class="input-label"
+          >プロフィール写真(API無し)</label
+        >
+        <input id="upload-picture" class="upload-picture" type="file" />
+        <label for="upload-picture" class="bg-camera">
+          <font-awesome-icon
+            icon="camera"
+            class="icon-camera"
+          ></font-awesome-icon>
+        </label>
+        <BaseInput v-model="user.name" label="名前(必須)" placeholder="ぎーく太郎" type="text"></BaseInput>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
+import BaseInput from '@/components/BaseInput'
 export default {
-
-}
+  data() {
+    return {
+      user: {
+        name: ''
+      }
+    }
+  },
+  components: {
+    BaseInput
+  }
+};
 </script>
 
 <style lang="scss">
@@ -22,13 +54,70 @@ export default {
   background-color: #212121;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
+  &__center {
+    display: flex;
+    align-items: flex-end;
+  }
   &__title {
     color: white;
     font-size: 15px;
     font-family: 'SF Pro Text';
     font-weight: 400;
-    text-align: center;
-    margin-top: 53px;
+    margin: 15px auto;
+  }
+  &__left {
+    display: flex;
+    align-items: flex-end;
+  }
+  &__right {
+    display: flex;
+    align-items: flex-end;
+    justify-content: right;
   }
 }
+.icon {
+  color: #34aa22;
+}
+.icon-left {
+  font-size: 28px;
+  margin-left: 20px;
+  margin-bottom: 12px;
+}
+.icon-pen {
+  font-size: 20px;
+  margin-left: auto;
+  margin-right: 21px;
+  margin-bottom: 15px;
+}
+.sign-up-form {
+  padding: 80px 20px 0;
+}
+.upload-picture {
+  display: none;
+}
+.bg-camera {
+  width: 90px;
+  height: 90px;
+  border-radius: 50%;
+  margin: 0 auto;
+  display: block;
+  background-color: rgba($color: #000000, $alpha: 0.3);
+  position: relative;
+}
+.icon-camera {
+  color: white;
+  opacity: 0.6;
+  font-size: 24px;
+  position: absolute;
+  left: 50%;
+  top: 48%;
+  transform: translate(-50%, -50%);
+}
+.input-label {
+  display: inline-block;
+  font-size: 15px;
+  font-weight: 600;
+  margin-bottom: 6px;
+}
+
 </style>
