1585fa6b445e5278d5d89a6830c6fe31eb509a30
yuta-san765 <nitoplus765@gmail.com>
2020-05-05 11:40:06 +0900

    installed fontawesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faAngleLeft, faCamera, faTimes, faPen, faEnvelope, faCog, faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faAngleLeft, faCamera, faTimes, faPen, faEnvelope, faCog, faUser)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 Vue.config.productionTip = false
 
